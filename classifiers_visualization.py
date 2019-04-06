2c8711c8d6f9b6f773835b22fd90fab0a5d2029c
Zackhardtoname <zackL@bu.edu>
2019-04-06 14:13:58 -0400

    uploaded more images

--- classifiers_visualization.py
+++ classifiers_visualization.py
@@ -1,21 +1,21 @@
 from sklearn.externals import joblib
 from sklearn.tree import export_graphviz
 from configs import feature_names
+from subprocess import call
 
 clf = joblib.load("./models/RandomForestClassifier.pkl")
-estimator = clf.estimators_[0]
 
-# Export as dot file
-export_graphviz(estimator, out_file='tree.dot',
-                feature_names = feature_names,
-                class_names = ["In", "Out"],
-                rounded = True, proportion = False,
-                precision = 2, filled = True)
+for i in range(len(clf.estimators_)):
+    # Export as dot file
+    export_graphviz(clf.estimators_[i], out_file=f'./trees/tree{i}.dot',
+                    feature_names = feature_names,
+                    class_names = ["In", "Out"],
+                    rounded = True, proportion = False,
+                    precision = 2, filled = True)
 
-# Convert to png using system command (requires Graphviz)
-from subprocess import call
-call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png', '-Gdpi=600'])
+    # Convert to png using system command (requires Graphviz)
+    call(['dot', '-Tpng', f'./trees/tree{i}.dot', '-o', f'./trees/tree{i}.png', '-Gdpi=600'])
 
-# Display in jupyter notebook
-from IPython.display import Image
-Image(filename = 'tree.png')
+    # # Display in jupyter notebook
+    # from IPython.display import Image
+    # Image(filename = 'tree.png')
--- processing_data.py
+++ processing_data.py
@@ -16,42 +16,11 @@ from sklearn.preprocessing import Imputer
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 
-X = pd.read_csv("./data_loader/data/training_data/player_stats_raw_features.csv")
+# X = pd.read_csv("./data_loader/data/training_data/player_stats_raw_features.csv")
 
 from data_loader.Draft import Draft
 from data_loader.Applicants import Applicants
-draft = Draft()
-applicants = Applicants()
-
-player_box = pd.read_csv("./data_loader/data/ncaa/player_box.csv")
-player_info = pd.read_csv("./data_loader/data/ncaa/player_info.csv")
-
-# Filter out unwanted values
-player_box = player_box[player_box['player_id']!=-101]
-player_box = player_box[~player_box['player_id'].isnull()]
-player_box = player_box[player_box['season']!=None]
-player_box = player_box[player_box['season']!=2018]
-
-player_info = player_info[player_info['player_id']!=-101]
-player_info = player_info[~player_info['player_id'].isnull()]
-player_info = player_info[player_info['season']!=None]
-player_info = player_info[player_info['season']!=2018]
-player_info = player_info[~player_info['first_name'].isnull()]
-player_info = player_info[~player_info['last_name'].isnull()]
-
-# Combine the two datasets
-player_box["player_id_AND_season"] = player_box["player_id"].map(int).map(str) + "_AND_" + player_box["season"].map(int).map(str)
-player_info["player_id_AND_season"] = player_info["player_id"].map(int).map(str) + "_AND_" + player_info["season"].map(int).map(str)
-player_stats = pd.merge(player_box, player_info, on='player_id_AND_season', how='outer')
-player_stats = player_stats.dropna()
-
-# Get only drafted players
-player_stats = applicants.join(player_stats)
-
-# Add drafted categorical value
-player_stats = draft.join(player_stats)
-
-player_stats.to_csv('./data_loader/data/ncaa/player_stats.csv')
+X = pd.read_csv("./data_loader/data/training_data/player_stats_raw_features.csv")
 
 num_attribs = ['pts', 'fga', 'fga3', 'fgm', 'fgm3', 'fta', 'ftm', 'ast', 'blk', 'stl', 'dreb', 'oreb', 'reb', 'pf', 'tf', 'tov', 'mins_played', 'grade_level', 'division', 'height_in']
 cat_attribs = ["school", "conference", "position"]
